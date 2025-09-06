ad8a3ea8edfcb01c826d2049f23a518e56c035d5
GiorgosAthanasopoulos <spzdoom@gmail.com>
2025-09-06 20:10:48 +0300

    add template imgui+raylib app

---
+++ src/template.cpp
@@ -0,0 +1,43 @@
+#include "imgui.h"
+#include "raylib.h"
+#include "rlImGui.h"
+
+int main() {
+  SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+  InitWindow(800, 600, "TODO"); // TODO:
+  SetWindowMinSize(640, 480);
+  SetTargetFPS(30);
+  SetExitKey(KEY_NULL);
+  InitAudioDevice();
+
+  rlImGuiSetup(false);
+
+  ImGui::GetStyle().FontScaleMain = 2;
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(WHITE);
+
+    rlImGuiBegin();
+
+    if (IsWindowResized()) {
+      ImGui::SetNextWindowPos(ImVec2(0, 20));
+      ImGui::SetNextWindowSize(
+          ImVec2(GetRenderWidth(), GetRenderHeight() - 20));
+    }
+    if (ImGui::Begin("##empty")) {
+    }
+    ImGui::End();
+
+    rlImGuiEnd();
+
+    DrawFPS(0, 0);
+    EndDrawing();
+  }
+
+  rlImGuiShutdown();
+
+  CloseAudioDevice();
+  CloseWindow();
+  return 0;
+}
